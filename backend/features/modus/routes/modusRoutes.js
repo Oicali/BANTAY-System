7df02607be5ac1e11caf9046134ca8324b4241b2
Oicali <121882893+Oicali@users.noreply.github.com>
2026-03-14 16:02:08 +0800

    to update pa

--- backend/features/modus/routes/modusRoutes.js
+++ backend/features/modus/routes/modusRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { authenticate } = require("../../../shared/middleware/tokenMiddleware");
-const { getAllModus, getModusById, createModus, updateModus } = require("../controllers/modusController");
+const { getAllModus, getModusById, createModus, updateModus, deleteModus } = require("../controllers/modusController");
 
 router.get("/", authenticate, getAllModus);
 router.get("/:id", authenticate, getModusById);
 router.post("/", authenticate, createModus);
 router.patch("/:id", authenticate, updateModus);
+router.delete("/:id", authenticate, deleteModus);
 
 module.exports = router;
