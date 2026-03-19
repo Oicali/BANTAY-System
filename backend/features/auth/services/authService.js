f221408c25ed03414e5059e92003b6519d071a95
Oicali <121882893+Oicali@users.noreply.github.com>
2026-03-19 19:43:36 +0800

    update authService

--- backend/features/auth/services/authService.js
+++ backend/features/auth/services/authService.js
@@ -8,10 +8,15 @@ const pool = require("../../../config/database");
 
 // Email transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,          // true for 465, false for 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
