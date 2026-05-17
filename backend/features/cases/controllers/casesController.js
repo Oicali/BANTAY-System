da6c4cfadff1c95617e3824d55e9c0223c078707
jeromegopela <jeromegopela@gmail.com>
2026-05-17 17:35:52 +0800

    Update casesController.js

--- backend/features/cases/controllers/casesController.js
+++ backend/features/cases/controllers/casesController.js
@@ -481,9 +481,9 @@ const getCaseById = async (req, res) => {
     const isAdmin = req.user.role === "Administrator";
     const notes = await pool.query(
       `SELECT cn.id, cn.note, cn.note_date,
-to_char(cn.created_at AT TIME ZONE 'UTC', 'YYYY-MM-DD"T"HH24:MI:SS"Z"') AS created_at,
-to_char(cn.edited_at AT TIME ZONE 'UTC', 'YYYY-MM-DD"T"HH24:MI:SS"Z"') AS edited_at,
-to_char(cn.deleted_at AT TIME ZONE 'UTC', 'YYYY-MM-DD"T"HH24:MI:SS"Z"') AS deleted_at,
+to_char(cn.created_at, 'YYYY-MM-DD"T"HH24:MI:SS+08:00') AS created_at,
+to_char(cn.edited_at, 'YYYY-MM-DD"T"HH24:MI:SS+08:00') AS edited_at,
+to_char(cn.deleted_at, 'YYYY-MM-DD"T"HH24:MI:SS+08:00') AS deleted_at,
           cn.added_by_id,
           CONCAT(u.first_name, ' ', u.last_name) AS added_by_name
    FROM case_notes cn
