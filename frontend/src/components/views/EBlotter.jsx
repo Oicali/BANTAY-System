64c9ca75ba97b3576bfc85364812fb7e71df1c78
Oicali <121882893+Oicali@users.noreply.github.com>
2026-05-28 00:07:37 +0800

    export

--- frontend/src/components/views/EBlotter.jsx
+++ frontend/src/components/views/EBlotter.jsx
@@ -2911,7 +2911,7 @@ function EBlotter() {
               <polyline points="7 10 12 15 17 10" />
               <line x1="12" y1="15" x2="12" y2="3" />
             </svg>
-            Export PDF
+            Export
           </button>
 
           <button
